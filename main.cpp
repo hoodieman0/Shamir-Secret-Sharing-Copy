c105d45274c18f21c6ae3c51a88ff1429d6dd970
James Mok <hoodieman8@gmail.com>
2023-08-24 22:50:19 -0400

    docs: added url lecture notes in main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+// based on the following lecture notes:
+// https://www.cs.purdue.edu/homes/hmaji/teaching/Fall%202020/lectures/08.pdf
+// https://www.cs.purdue.edu/homes/hmaji/teaching/Fall%202020/lectures/09.pdf
+
 int main() {
     UnitTest_RunAll();
     return 0;
